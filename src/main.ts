c1b0f4ad7c195e3de2b6dadd3f265214e6be5156
vitalii boiko <vukelan@gmail.com>
2025-07-18 15:49:56 +0300

    add control animation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Assets, Sprite, Graphics } from 'pixi.js';
+import { Application, Assets, Sprite, Graphics, Ticker } from 'pixi.js';
 
 (async () => {
   // Create a new application
@@ -37,19 +37,56 @@ import { Application, Assets, Sprite, Graphics } from 'pixi.js';
   app.stage.addChildAt(unocard, 1);
   app.stage.addChildAt(sq, 2);
 
+  let isStarted: Ticker;
+  // build button unrelated to Pixi
+  {
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.style.position = 'absolute';
+    button.textContent = 'start move';
+    const appElement = document.getElementById('app');
+    appElement?.appendChild(button);
+
+    button.addEventListener('click', () => {
+      if (isStarted != undefined) {
+        if (!isStarted.started) {
+          isStarted.start();
+          return;
+        }
+
+        isStarted.stop();
+        return;
+      }
+      isStarted = app.ticker.add((time) => {
+        // Just for fun, let's rotate mr rabbit a little.
+        // * Delta is 1 if running at 100% performance *
+        // * Creates frame-independent transformation *
+        const p0 = unocard.position;
+        if (p0._x > app.screen.width / 2 && p0._y > app.screen.height / 2) {
+          unocard.position.set(app.screen.width / 4, app.screen.height / 4);
+        }
+        bunny.rotation += 0.1 * time.deltaTime;
+        unocard.rotation += 0.025 * time.deltaTime;
+        unocard.position.set(p0._x + 1, p0._y + 1);
+        const p = unocard.position;
+        sq.position.set(p._x - 5, p._y - 5);
+      });
+    });
+  }
+
   // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    const p0 = unocard.position;
-    if (p0._x > app.screen.width / 2 && p0._y > app.screen.height / 2) {
-      unocard.position.set(app.screen.width / 4, app.screen.height / 4);
-    }
-    bunny.rotation += 0.1 * time.deltaTime;
-    unocard.rotation += 0.025 * time.deltaTime;
-    unocard.position.set(p0._x + 1, p0._y + 1);
-    const p = unocard.position;
-    sq.position.set(p._x - 5, p._y - 5);
-  });
+  // app.ticker.add((time) => {
+  //   // Just for fun, let's rotate mr rabbit a little.
+  //   // * Delta is 1 if running at 100% performance *
+  //   // * Creates frame-independent transformation *
+  //   const p0 = unocard.position;
+  //   if (p0._x > app.screen.width / 2 && p0._y > app.screen.height / 2) {
+  //     unocard.position.set(app.screen.width / 4, app.screen.height / 4);
+  //   }
+  //   bunny.rotation += 0.1 * time.deltaTime;
+  //   unocard.rotation += 0.025 * time.deltaTime;
+  //   unocard.position.set(p0._x + 1, p0._y + 1);
+  //   const p = unocard.position;
+  //   sq.position.set(p._x - 5, p._y - 5);
+  // });
 })();
