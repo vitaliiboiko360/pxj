d1fec3f60502e368b803bb41599b6a58c76ee26d
vitalii boiko <vukelan@gmail.com>
2025-07-19 20:07:50 +0300

    rewrite as class

--- src/main.ts
+++ src/main.ts
@@ -1,102 +1,112 @@
-import { Application, Assets, Sprite, Graphics, Ticker } from 'pixi.js';
+'use strict';
+import { Application, Assets, Sprite, Graphics, Ticker, Color } from 'pixi.js';
 
-(async () => {
-  // Create a new application
-  const app = new Application();
+declare global {
+  interface Window {
+    pixi: PixiAnimator;
+  }
+}
 
-  // Initialize the application
-  await app.init({ background: '#1099bb', resizeTo: window });
+class PixiAnimator {
+  bunny;
+  unocard;
+  sq;
+  isInitialized = false;
+  app;
+  isStarted;
 
-  // Append the application canvas to the document body
-  document.getElementById('pixi-container')!.appendChild(app.canvas);
+  async init() {
+    if (this.isInitialized) {
+      console.log('already initialized');
+      return;
+    }
 
-  // Load the bunny texture
-  const texture = await Assets.load('/assets/bunny.png');
-  const texture2 = await Assets.load('/assets/uno_card.svg');
+    this.app = new Application();
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
-  const unocard = new Sprite(texture2);
+    // Initialize the application
+    await this.app.init({
+      backgroundAlpha: 0,
+      background: new Color({ r: 255, g: 0, b: 0, a: 0 }),
+      resizeTo: window,
+    });
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-  unocard.anchor.set(0.5);
+    // Append the application canvas to the document body
+    document.getElementById('pixi-container')!.appendChild(this.app.canvas);
+    console.log(this.app);
+    const texture = await Assets.load('/assets/bunny.png');
+    const texture2 = await Assets.load('/assets/uno_card.svg');
+    this.bunny = new Sprite(texture);
+    this.unocard = new Sprite(texture2);
 
-  unocard.scale.set(0.5, 0.5);
+    this.bunny.anchor.set(0.5);
+    this.unocard.anchor.set(0.5);
 
-  const sq = new Graphics().rect(0, 0, 10, 10).fill('red');
+    this.unocard.scale.set(0.5, 0.5);
 
-  sq.position.set(app.screen.width / 4, app.screen.height / 4);
+    this.sq = new Graphics().rect(0, 0, 10, 10).fill('red');
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-  unocard.position.set(app.screen.width / 4, app.screen.height / 4);
+    this.sq.position.set(this.app.screen.width / 4, this.app.screen.height / 4);
 
-  const center_x = app.screen.width / 2;
-  const center_y = app.screen.height / 2;
+    this.bunny.position.set(
+      this.app.screen.width / 2,
+      this.app.screen.height / 2
+    );
+    this.unocard.position.set(
+      this.app.screen.width / 4,
+      this.app.screen.height / 4
+    );
 
-  // Add the bunny to the stage
-  app.stage.addChildAt(bunny, 0);
-  app.stage.addChildAt(unocard, 1);
-  app.stage.addChildAt(sq, 2);
+    this.app.stage.addChildAt(this.bunny, 0);
+    this.app.stage.addChildAt(this.unocard, 1);
+    this.app.stage.addChildAt(this.sq, 2);
 
-  let isStarted: Ticker;
-  // build button unrelated to Pixi
-  {
-    const button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    button.style.position = 'absolute';
-    button.textContent = 'start move';
-    const appElement = document.getElementById('app');
-    appElement?.appendChild(button);
+    this.isInitialized = true;
+  }
 
-    button.addEventListener('click', () => {
-      if (isStarted != undefined) {
-        if (!isStarted.started) {
-          isStarted.start();
-          return;
-        }
-
-        isStarted.stop();
+  startStopMoving() {
+    if (this.isStarted != undefined) {
+      if (!this.isStarted.started) {
+        this.isStarted.start();
         return;
       }
-      const radius = 200;
-      let angle = 0;
-      isStarted = app.ticker.add((time) => {
-        // Just for fun, let's rotate mr rabbit a little.
-        // * Delta is 1 if running at 100% performance *
-        // * Creates frame-independent transformation *
 
-        bunny.rotation += 0.1 * time.deltaTime;
+      this.isStarted.stop();
+      return;
+    }
+    const radius = 200;
+    let angle = 0;
+    this.isStarted = this.app.ticker.add((time) => {
+      this.bunny.rotation += 0.1 * time.deltaTime;
 
-        unocard.rotation += 0.025 * time.deltaTime;
+      this.unocard.rotation += 0.025 * time.deltaTime;
 
-        angle = (angle + 0.01) % (Math.PI * 2);
+      angle = (angle + 0.01) % (Math.PI * 2);
 
-        unocard.position.set(
-          center_x + Math.cos(angle) * radius,
-          center_y + Math.sin(angle) * radius
-        );
+      this.unocard.position.set(
+        center_x + Math.cos(angle) * radius,
+        center_y + Math.sin(angle) * radius
+      );
 
-        const p = unocard.position;
-        sq.position.set(p._x - 5, p._y - 5);
-      });
+      const p = this.unocard.position;
+      this.sq.position.set(p._x - 5, p._y - 5);
     });
   }
 
-  // Listen for animate update
-  // app.ticker.add((time) => {
-  //   // Just for fun, let's rotate mr rabbit a little.
-  //   // * Delta is 1 if running at 100% performance *
-  //   // * Creates frame-independent transformation *
-  //   const p0 = unocard.position;
-  //   if (p0._x > app.screen.width / 2 && p0._y > app.screen.height / 2) {
-  //     unocard.position.set(app.screen.width / 4, app.screen.height / 4);
-  //   }
-  //   bunny.rotation += 0.1 * time.deltaTime;
-  //   unocard.rotation += 0.025 * time.deltaTime;
-  //   unocard.position.set(p0._x + 1, p0._y + 1);
-  //   const p = unocard.position;
-  //   sq.position.set(p._x - 5, p._y - 5);
-  // });
-})();
+  centerCat() {
+    this.bunny.position.set(center_x, center_y);
+  }
+}
+
+let center_x = window.innerWidth / 2;
+let center_y = window.innerHeight / 2;
+
+window.addEventListener('resize', () => {
+  center_x = window.innerWidth / 2;
+  center_y = window.innerHeight / 2;
+
+  if (window.pixi.isInitialized) {
+    window.pixi.centerCat();
+  }
+});
+
+window.pixi = new PixiAnimator();
